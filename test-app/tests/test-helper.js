e53566e100c846f9ff881060bdda4285b722994d
Jonathan Johnson <jon.johnson@ucsf.edu>
2025-03-16 21:22:58 -0700

    Apply v2 addon blueprint
    Ran `pnpx ember-cli@latest addon @ember/render-modifiers -b
    @embroider/addon-blueprint --pnpm --skip-install --skip-git --typescript
    --addon-location=ember-render-modifiers` and created everything from
    scratch then copied in modifiers, tests, and the PNPM version setting
    from ci.yml

---
+++ test-app/tests/test-helper.js
@@ -0,0 +1,22 @@
+import Application from 'test-app/app';
+import config from 'test-app/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { loadTests } from 'ember-qunit/test-loader';
+import { start, setupEmberOnerrorValidation } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+setupEmberOnerrorValidation();
+loadTests();
+start();
+
+QUnit.assert.namedArgsEqual = function (actual, expected, message) {
+  // this is needed because older versions of Ember pass an `EmptyObject`
+  // based object and QUnit fails due to the prototypes not matching
+  const sanitizedActual = Object.assign({}, actual);
+
+  this.deepEqual(sanitizedActual, expected, message);
+};
